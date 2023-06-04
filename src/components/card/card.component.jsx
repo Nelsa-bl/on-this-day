5dda62f1151c4e0e4ec9d9a7e8804ad9f70c9d54
Nelsa-bl <sanelbabij@gmail.com>
2023-06-04 18:21:15 +0200

    fixed API endpoint

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,18 +4,18 @@ import './card.style.scss';
 const Card = ({ data }) => {
   return (
     <div className='card-container'>
-      <a href={data.pages[0]?.content_urls?.mobile?.page}>
+      <a href={data.pages[1]?.content_urls?.mobile?.page}>
         <h4>{data.year}</h4>
-        <span className='name'>{data.pages[0]?.normalizedtitle}</span>
-        {data.pages[0]?.thumbnail?.source && (
+        <span className='name'>{data.pages[1]?.titles?.normalized}</span>
+        {data.pages[1]?.thumbnail?.source && (
           <img
             className='image'
-            alt={data.pages[0]?.normalizedtitle}
-            src={data.pages[0]?.thumbnail?.source}
+            alt={data.pages[1]?.normalizedtitle}
+            src={data.pages[1]?.thumbnail?.source}
           />
         )}
 
-        <span className='desc'>{data.pages[0]?.description}</span>
+        <span className='desc'>{data.pages[1]?.description}</span>
         <p>{data.text}</p>
       </a>
     </div>
